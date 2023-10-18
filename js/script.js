7fc31a0d98fc0ed21c33a7c4be767b46ba696ae7
Aaron Varga <147112718+thenihilistdev@users.noreply.github.com>
2023-10-18 14:32:19 -0400

    Styling updates, added some JS functionality

---
+++ js/script.js
@@ -0,0 +1,95 @@
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let num3 = document.getElementById("num3");
+let num4 = document.getElementById("num4");
+let num5 = document.getElementById("num5");
+
+num1.addEventListener("click", function() {
+  num1.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  num1.style.color = 'hsl(0, 0%, 100%)';
+
+  // Set all others back to default inactive
+  num2.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num2.style.color = 'hsl(217, 12%, 63%)';
+
+  num3.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num3.style.color = 'hsl(217, 12%, 63%)';
+
+  num4.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num4.style.color = 'hsl(217, 12%, 63%)';
+
+  num5.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num5.style.color = 'hsl(217, 12%, 63%)';
+});
+
+num2.addEventListener("click", function() {
+  num2.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  num2.style.color = 'hsl(0, 0%, 100%)';
+
+  // Set all others back to default inactive
+  num1.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num1.style.color = 'hsl(217, 12%, 63%)';
+
+  num3.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num3.style.color = 'hsl(217, 12%, 63%)';
+
+  num4.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num4.style.color = 'hsl(217, 12%, 63%)';
+
+  num5.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num5.style.color = 'hsl(217, 12%, 63%)';
+});
+
+num3.addEventListener("click", function() {
+  num3.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  num3.style.color = 'hsl(0, 0%, 100%)';
+
+  // Set all others back to default inactive
+  num1.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num1.style.color = 'hsl(217, 12%, 63%)';
+
+  num2.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num2.style.color = 'hsl(217, 12%, 63%)';
+
+  num4.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num4.style.color = 'hsl(217, 12%, 63%)';
+
+  num5.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num5.style.color = 'hsl(217, 12%, 63%)';
+});
+
+num4.addEventListener("click", function() {
+  num4.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  num4.style.color = 'hsl(0, 0%, 100%)';
+
+  // Set all others back to default inactive
+  num1.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num1.style.color = 'hsl(217, 12%, 63%)';
+
+  num2.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num2.style.color = 'hsl(217, 12%, 63%)';
+
+  num3.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num3.style.color = 'hsl(217, 12%, 63%)';
+
+  num5.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num5.style.color = 'hsl(217, 12%, 63%)';
+});
+
+num5.addEventListener("click", function() {
+  num5.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  num5.style.color = 'hsl(0, 0%, 100%)';
+
+  // Set all others back to default inactive
+  num1.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num1.style.color = 'hsl(217, 12%, 63%)';
+
+  num2.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num2.style.color = 'hsl(217, 12%, 63%)';
+
+  num3.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num3.style.color = 'hsl(217, 12%, 63%)';
+
+  num4.style.backgroundColor = 'rgba(255,255,255,.1)';
+  num4.style.color = 'hsl(217, 12%, 63%)';
+});
